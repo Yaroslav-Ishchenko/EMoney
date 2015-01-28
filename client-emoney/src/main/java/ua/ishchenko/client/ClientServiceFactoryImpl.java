d168942fcec1f8948952dc9bc6ac81f34fc7053c
Yaroslav-Ishchenko <jaros18@mail.ru>
2015-01-28 21:28:20 +0200

    Added service factories

---
+++ client-emoney/src/main/java/ua/ishchenko/client/ClientServiceFactoryImpl.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package ua.ishchenko.client;
+
+import org.fusesource.restygwt.client.Defaults;
+
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Sample implementation of {@link ClientFactory}.
+ */
+public class ClientServiceFactoryImpl implements ClientServiceFactory
+{
+
+   private String serviceBaseUrl;
+   public static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss Z";
+
+   private void ensureRestyIsConfigured()
+   {
+      if (serviceBaseUrl == null)
+      {
+         serviceBaseUrl = GWT.getHostPageBaseURL() + "aml/";
+         serviceBaseUrl = serviceBaseUrl.replaceFirst("/lmc", "");
+         Defaults.setServiceRoot(serviceBaseUrl);
+         Defaults.setDateFormat(DEFAULT_DATETIME_FORMAT);
+      }
+   }
+//
+//   @Override
+//   public UserService getUserService()
+//   {
+//      ensureRestyIsConfigured();
+//      return GWT.create(UserService.class);
+//   }
+}
